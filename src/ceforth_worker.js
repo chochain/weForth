7229a5f48d9910814c4852c2053245e25f91e757
Chochain Lee <chochain@yahoo.com>
2022-11-23 16:10:15 -0800

    ~/src/ceforth_worker.js add send_ss,send_dict and request dispatcher

--- src/ceforth_worker.js
+++ src/ceforth_worker.js
@@ -3,10 +3,36 @@
 /// @brief weForth - worker proxy to ceforth.js (called by ceforth.html)
 ///
 Module = {
-    print: postMessage
+    print: (e)=>postMessage([ 'cmd', e ])
+}
+function send_ss() {
+    let ex  = Module.asm
+    let len = ex.vm_ss_idx()
+    let ss  = new Int32Array(Module.asm.memory.buffer, ex.vm_ss(), len)
+    let top = new Int32Array(Module.asm.memory.buffer, ex.top, 1)
+    let div = [ top[0] ]
+    for (let i = len - 1; i >= 0; --i) {
+        div.push(ss[i])
+    }
+    postMessage([ 'ss', div ])
+}
+function send_dict() {
+    let ex  = Module.asm          
+    let len = ex.vm_dict_idx()
+    let dict= Module.cwrap('vm_dict', 'string', ['number'])
+    let div = []
+    for (let i = len - 1; i >= 0; --i) {
+        div.push(dict(i))
+    }
+    postMessage([ 'dc', div ])
 }
 self.onmessage = function(e) {                    /// * link worker input port
     let forth = Module.cwrap('forth', null, ['number', 'string'])
-    forth(0, e.data[0])                           /// * call Forth in C/C++
+    switch (e.data[0]) {
+    case 'cmd': forth(0, e.data[1]); break        /// * call Forth in C/C++
+    case 'ss' : send_ss();           break
+    case 'dc' : send_dict();         break
+    default: postMessage('unknown type');
+    }
 }
 importScripts('ceforth.js')                       /// * load js emscripten created
