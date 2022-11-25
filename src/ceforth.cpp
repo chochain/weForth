be2a1201e4e6d45c45e2e84a10784e8d9916d5dd
Chochain Lee <chochain@yahoo.com>
2022-11-24 16:25:06 -0800

    ~/src/ceforth.cpp bump to v1.2

--- src/ceforth.cpp
+++ src/ceforth.cpp
@@ -9,7 +9,7 @@
 ///
 #define APP_NAME         "weForth"
 #define MAJOR_VERSION    "1"
-#define MINOR_VERSION    "0"
+#define MINOR_VERSION    "2"
 ///==============================================================================
 ///
 /// global memory blocks
