350121cb501f4b62768a43dc10d4d35181e98bb7
gnii <chochain@yahoo.com>
2022-12-26 19:29:44 +0800

    ~/src/weforth_worker.js add colon word handler

--- src/weforth_worker.js
+++ src/weforth_worker.js
@@ -9,7 +9,7 @@ var vm_dict_len = 0
 
 importScripts('weforth_helper.js')             /// * vocabulary handler
 
-function show_ss() {
+function send_ss() {
     let ex  = Module.asm
     let base= ex.vm_base()
     let len = ex.vm_ss_idx()
@@ -20,26 +20,32 @@ function show_ss() {
     div.push(top[0].toString(base))
     postMessage([ 'ss', div ])
 }
-function show_dict() {
+function send_dict() {
     let ex  = Module.asm          
     let len = ex.vm_dict_idx()
-    if (vm_dict_len == len) return
+    if (vm_dict_len == len) return             /// * dict no change, skip
     
     vm_dict_len = len
     let dict = Module.cwrap('vm_dict', 'string', ['number'])
-    let nlst = []
+    let nlst = []                              ///< built-in words list
+    let clst = null                            ///< colon word list
     for (let i = 0; i < len; ++i) {
         let nm = dict(i)
-        if (nm[0]!='_') nlst.push(nm)
+        if (clst) clst.push(nm)
+        else {
+            if (nm[0]!='_') nlst.push(nm)
+            if (nm == 'boot') clst = []
+        }
     }
     postMessage([ 'dc', _voc_tree(nlst) ])
+    postMessage([ 'us', _colon_words(clst) ])
 }
 self.onmessage = function(e) {                    /// * link worker input port
     let forth = Module.cwrap('forth', null, ['number', 'string'])
     switch (e.data[0]) {
     case 'cmd': forth(0, e.data[1]); break        /// * call Forth in C/C++
-    case 'ss' : show_ss();           break
-    case 'dc' : show_dict();         break
+    case 'ss' : send_ss();           break
+    case 'dc' : send_dict();         break
     default: postMessage('unknown type');
     }
 }
