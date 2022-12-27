5084888788a941bf7ce84974a9c024f28752ba9d
gnii <chochain@yahoo.com>
2022-12-27 15:28:37 +0800

    ~/weforth_helper,weforth_worker.js clean interface; add mem export

--- src/weforth_worker.js
+++ src/weforth_worker.js
@@ -6,6 +6,7 @@ Module = {
     print: (e)=>postMessage([ 'cmd', e ])
 }
 var vm_dict_len = 0
+var vm_mem_addr = 0
 
 importScripts('weforth_helper.js')             /// * vocabulary handler
 
@@ -18,11 +19,10 @@ function send_ss() {
     let div = []
     ss.forEach(v=>div.push(v.toString(base)))
     div.push(top[0].toString(base))
-    postMessage([ 'ss', div ])
+    postMessage([ 'ss', '[ ' + div.join(' ') + ' ]' ])
 }
 function send_dict() {
-    let ex  = Module.asm          
-    let len = ex.vm_dict_idx()
+    let len = Module.asm.vm_dict_idx()
     if (vm_dict_len == len) return             /// * dict no change, skip
     
     vm_dict_len = len
@@ -37,15 +37,22 @@ function send_dict() {
             if (nm == 'boot') clst = []
         }
     }
-    postMessage([ 'dc', _voc_tree(nlst) ])
-    postMessage([ 'us', _colon_words(clst) ])
+    postMessage([ 'dc', voc_tree(nlst) ])
+    postMessage([ 'us', colon_words(clst) ])
+}
+function send_mem(off, len) {
+    let adr = Module.asm.vm_mem() + off
+    let mem = new Uint8Array(Module.asm.memory.buffer, adr, len)
+    postMessage([ 'mm', mem ])
 }
 self.onmessage = function(e) {                    /// * link worker input port
     let forth = Module.cwrap('forth', null, ['number', 'string'])
-    switch (e.data[0]) {
-    case 'cmd': forth(0, e.data[1]); break        /// * call Forth in C/C++
-    case 'ss' : send_ss();           break
-    case 'dc' : send_dict();         break
+    let k = e.data[0], v = e.data[1]
+    switch (k) {
+    case 'cmd': forth(0, v);          break       /// * call Forth in C/C++
+    case 'ss' : send_ss();            break
+    case 'dc' : send_dict();          break
+    case 'mm' : send_mem(v[0], v[1]); break
     default: postMessage('unknown type');
     }
 }
