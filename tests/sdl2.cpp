f75943b41036a1290e70d9c272ef911cfe16dffc
gnii <chochain@yahoo.com>
2024-02-04 07:50:10 +0800

    ~/tests/sdl2.cpp rename rndr,rect=>rn,vp (ready for interface change)

--- tests/sdl2.cpp
+++ tests/sdl2.cpp
@@ -15,20 +15,20 @@
 ///
 ///> global control variables
 ///
-bool     g_run   = true;
-TTF_Font *g_font = NULL;
+bool     g_run    = true;
+TTF_Font *g_font  = NULL;
 ///
 ///> Rectangle textured tile class (for image or solid color)
 ///
 struct Tile {
-    SDL_Renderer *rndr;          // pointer to renderer
+    SDL_Renderer *rn;            // pointer to renderer
+    SDL_Rect     vp;             // rectangle view port to be drawn upon
     SDL_Color    c4;             // set draw color if defined
     bool         c4_set = false; // color set
-    SDL_Rect     rect;           // rectangle to be drawn upon
     double       ang = 0.0;
     
-    Tile(SDL_Renderer *rndr, int x, int y, int w, int h) : rndr(rndr) {
-        rect.x = x; rect.y = y, rect.w = w; rect.h = h;
+    Tile(SDL_Renderer *rndr, int x, int y, int w, int h) : rn(rndr) {
+        vp.x = x; vp.y = y, vp.w = w; vp.h = h;
     }
     Tile *set_color(SDL_Color c) {
         c4     = c;
@@ -39,9 +39,9 @@ struct Tile {
     virtual int  load(const char *str=NULL, SDL_Color *c=NULL) { return 0; }
     virtual int  render(SDL_Rect *clip=NULL) {
         if (c4_set) {
-            SDL_SetRenderDrawColor(rndr, c4.r, c4.g, c4.b, c4.a);
+            SDL_SetRenderDrawColor(rn, c4.r, c4.g, c4.b, c4.a);
         }
-        SDL_RenderFillRect(rndr, &rect);    // fill rectangle
+        SDL_RenderFillRect(rn, &vp);    // fill rectangle
         return 0;
     }
 };
@@ -54,7 +54,7 @@ struct Image : Tile {
 
     int replace_tex_then_free(SDL_Surface *img) {      // boiler plate
         free();
-        tex = SDL_CreateTextureFromSurface(rndr, img); // convert img to GPU texture
+        tex = SDL_CreateTextureFromSurface(rn, img);   // convert img to GPU texture
         SDL_FreeSurface(img);
         CHK(!tex, SDL);
         return 0;
@@ -65,8 +65,8 @@ struct Image : Tile {
         SDL_Surface *img = IMG_Load(fname);
         CHK(!img, IMG);
         
-        rect.w = img->w;                               // adjust image size
-        rect.h = img->h;
+        vp.w = img->w;                                 // adjust image size
+        vp.h = img->h;
         
         if (key) {
             SDL_Color &k = *key;                       // use reference
@@ -77,12 +77,12 @@ struct Image : Tile {
     }
     virtual int render(SDL_Rect *clip=NULL) override {
         if (c4_set) {
-            SDL_SetRenderDrawColor(rndr, c4.r, c4.g, c4.b, c4.a);
+            SDL_SetRenderDrawColor(rn, c4.r, c4.g, c4.b, c4.a);
         }
         SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);    // can use other blending mode
 //          SDL_RenderCopy(rndr, tex, NULL, &rect);           // entire texture
 //          SDL_RenderCopy(rndr, tex, NULL, NULL);            // stretch to entire viewport
-        SDL_RenderCopyEx(rndr, tex, NULL, &rect,
+        SDL_RenderCopyEx(rn, tex, NULL, &vp,
                          ang, NULL /* center */, SDL_FLIP_NONE);
         return 0;
     }
@@ -95,13 +95,13 @@ struct Canvas : Image {
 
     virtual int  load(const char *dummy=NULL, SDL_Color *key=NULL) override {
         SDL_Surface *rgb =
-            SDL_CreateRGBSurface(0, rect.w, rect.h, 32, 0, 0, 0, 0);
+            SDL_CreateRGBSurface(0, vp.w, vp.h, 32, 0, 0, 0, 0);
         CHK(!rgb, SDL);
         
         if (SDL_MUSTLOCK(rgb)) SDL_LockSurface(rgb);
         Uint32 *px = (Uint32*)rgb->pixels;
-        for (int y=0; y < rect.h; y++) {
-            for (int x=0; x < rect.w; x++) {
+        for (int y=0; y < vp.h; y++) {
+            for (int x=0; x < vp.w; x++) {
                 *px++ = y | (x << 8);
             }
         }
@@ -118,7 +118,7 @@ struct Text : Image {
     std::stringstream stime;
     
     Text(SDL_Renderer *rndr, int x, int y, int w=0, int h=0) : Image(rndr, x, y, w, h) {
-        SDL_Color black = {0,0,0,0xff};
+        SDL_Color black = { 0, 0, 0, 0xff};
         set_color(black);                                    // default to black
     }
     int load(const char *str, SDL_Color *c=NULL) override {  // text string with color
@@ -143,8 +143,8 @@ struct Text : Image {
             g_font, stime.str().c_str(), c4);
         CHK(!txt, TTF);
         
-        rect.w = txt->w;
-        rect.h = txt->h;
+        vp.w = txt->w;
+        vp.h = txt->h;
 
         return replace_tex_then_free(txt) || Image::render();
     }
@@ -155,30 +155,30 @@ struct Text : Image {
 ///
 struct Context {
     SDL_Window   *win;
-    SDL_Renderer *rndr;
-    SDL_Rect     vport;
+    SDL_Renderer *rn;
+    SDL_Rect     vp;
     SDL_Color    bg;
     
     Uint8        r = 0x80, a = 0x80;         // default colors
     std::vector<Tile*> tile;                 // polymorphic pointers
     Tile         *img, *sq;                  // specialized
 
-    void init(SDL_Window *wn, SDL_Renderer *rn, SDL_Color *c=NULL) {
+    void init(SDL_Window *w, SDL_Renderer *r, SDL_Color *c=NULL) {
         SDL_Color white = { 0xff, 0xff, 0xff, 0xff };
-        win = wn; rndr = rn; bg = c ? *c : white;
+        win = w; rn = r; bg = c ? *c : white;
         
-        SDL_SetRenderDrawBlendMode(rndr, SDL_BLENDMODE_BLEND);  // for alpha blending
+        SDL_SetRenderDrawBlendMode(rn, SDL_BLENDMODE_BLEND);  // for alpha blending
     }
     void render() {
-        SDL_SetRenderDrawColor(rndr, bg.r, bg.g, bg.b, bg.a);   // shade the background
-        SDL_RenderClear(rndr);
+        SDL_SetRenderDrawColor(rn, bg.r, bg.g, bg.b, bg.a);   // shade the background
+        SDL_RenderClear(rn);
         for (int i=0; i < tile.size(); i++) {
             tile[i]->render();
         }
-        SDL_RenderPresent(rndr);                                // update screen
+        SDL_RenderPresent(rn);                                // update screen
     }
     void free() {
-        SDL_DestroyRenderer(rndr);
+        SDL_DestroyRenderer(rn);
         for (int i=0; i < tile.size(); i++) {
             tile[i]->free();
         }
@@ -192,14 +192,14 @@ struct Context {
 void run(void *arg) {
     Context   &ctx = *static_cast<Context*>(arg);
     SDL_Event ev;
-    SDL_Rect  &img = ctx.img->rect;
+    SDL_Rect  &img = ctx.img->vp;
     while (SDL_PollEvent(&ev)) {
         switch (ev.type) {
         case SDL_QUIT: exit(0); /* force exit */   break;
         case SDL_MOUSEBUTTONDOWN: img.w <<= 1;     break;
         case SDL_MOUSEBUTTONUP:   img.w >>= 1;     break;
         case SDL_KEYDOWN: {
-            SDL_Rect &sq = ctx.sq->rect;
+            SDL_Rect &sq = ctx.sq->vp;
             switch(ev.key.keysym.sym) {
             case SDLK_UP:    sq.y -= 20;           break;
             case SDLK_DOWN:  sq.y += 20;           break;
@@ -226,21 +226,20 @@ void run(void *arg) {
 ///
 ///> SDL setup
 ///
-int setup(Context &ctx, const char *title, SDL_Rect vp) {
+int setup(Context *ctx, const char *title, SDL_Rect *vp, SDL_Color *bg) {
     CHK(IMG_Init(IMG_INIT_PNG)==-1, IMG);
     CHK(TTF_Init()==-1, TTF);
     
-    g_font = TTF_OpenFont("tests/assets/FreeSans.ttf", 48);
+    g_font = TTF_OpenFont("tests/assets/FreeSans.ttf", 48);   // global
     CHK(!g_font, TTF);
 
     SDL_Window *win = SDL_CreateWindow(
-        title, vp.x, vp.y, vp.w, vp.h,
+        title, vp->x, vp->y, vp->w, vp->h,
         SDL_WINDOW_SHOWN
         );
     SDL_Renderer *rndr = SDL_CreateRenderer(win, -1, 0);
-    SDL_Color    bg    = { 0xf0, 0xff, 0xe0, 0x80 };
     
-    ctx.init(win, rndr, &bg);
+    ctx->init(win, rndr, bg);
 
     return 0;
 }
@@ -248,17 +247,18 @@ int setup(Context &ctx, const char *title, SDL_Rect vp) {
 ///> SDL core
 ///
 int test_sdl2(Context &ctx, const char *text, const char *fname) {
-    SDL_Color key = {0xff, 0xff, 0xff, 0xff};          // key on white (as transparent)
-    SDL_Color red = {0xff, 0x0,  0x0,  0xff};
+    SDL_Renderer *rn = ctx.rn;
+    SDL_Color    key = {0xff, 0xff, 0xff, 0xff};       // key on white (as transparent)
+    SDL_Color    red = {0xff, 0x0,  0x0,  0xff};
 
-    ctx.sq  = new Tile(ctx.rndr, 400, 80, 200, 200);   // initialize square
-    ctx.img = new Image(ctx.rndr, 160, 160);           // initialize image
+    ctx.sq  = new Tile(rn, 400, 80, 200, 200);         // initialize square
+    ctx.img = new Image(rn, 160, 160);                 // initialize image
     if (ctx.img->load(fname, &key)) return 1;
 
-    Tile *txt = new Text(ctx.rndr, 60, 120);           // initialize text
+    Tile *txt = new Text(rn, 60, 120);                 // initialize text
     if (txt->load(text, &red)) return 1;               // text default background transparent
     
-    Tile *cnv = new Canvas(ctx.rndr, 240, 100, 256, 256);
+    Tile *cnv = new Canvas(rn, 240, 100, 256, 256);
     if (cnv->load()) return 1;
 
     ctx.tile.push_back(cnv);
@@ -271,10 +271,10 @@ int test_sdl2(Context &ctx, const char *text, const char *fname) {
 ///
 /// SDL teardown (not called by WASM)
 ///
-void teardown(Context &ctx) {
+void teardown(Context *ctx) {
 #ifndef EMSCRIPTEN
     printf("SDL shutting down...\n");
-    ctx.free();
+    ctx->free();
     printf("%s done.\n", __FILE__);
 #endif
 }
@@ -283,11 +283,12 @@ int main(int argc, char** argv) {
     const char *title = "SDL2 works";
     const char *text  = "Hello Owl!";
     const char *fname = "tests/assets/owl.png";
-    SDL_Rect    vport = {50, 30, 640, 480};
+    SDL_Rect    wsize = { 50, 30, 640, 480 };
+    SDL_Color   bg    = { 0xf0, 0xff, 0xe0, 0x80 };
     Context ctx;
 
     SDL_Init(SDL_INIT_VIDEO);
-    if (setup(ctx, title, vport)) return -1;
+    if (setup(&ctx, title, &wsize, &bg)) return -1;
     
     if (test_sdl2(ctx, text, fname)) return -1;
     
@@ -297,7 +298,7 @@ int main(int argc, char** argv) {
     while (g_run) { run(&ctx); }
 #endif 
     
-    teardown(ctx);
+    teardown(&ctx);
     SDL_Quit();
   
     return 0;
