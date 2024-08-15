53b8dc70de2aea7be6abb9bdba9572c085f72182
gnii <chochain@yahoo.com>
2024-08-16 07:29:06 +0800

    ~/docs/ref jolt update

--- docs/ref/weforth_jolt.js
+++ docs/ref/weforth_jolt.js
@@ -39,18 +39,6 @@ function build_mesh(n, sz, h) {    // nxn, sz=tileSize, h:max_height
 
     return shape
 }
-function get_q4(
-    x=Math.random(),
-    y=Math.random(),
-    z=Math.random(),
-    w=2*Math.PI*Math.random()) {
-    let v3 = new Jolt.Vec3(0.001+x, y, z).Normalized()
-    let q4 = (x==0 && y==0 && z==0)
-        ? new Jolt.Quat(0, 0, 0, 1)
-        : Jolt.Quat.prototype.sRotation(v3, w)
-    Jolt.destroy(v3)
-    return q4
-}
 function get_shape(t, v=null) {
     const rx = ()=>0.5 + Math.random()
     let x = v || (t==0 ? [ 30, 1, 0.8 ] : [ rx(), rx(), rx() ])
@@ -80,7 +68,14 @@ function get_shape(t, v=null) {
         shape = new Jolt.CapsuleShape(l, r1, 0.05, null)
         break
     }
-    case 5: {                       // Static compound shape
+    case 5: {                       // tapered capsule
+        let l = x[0], r1 = x[1] * 0.5, r2 = x[2] * 0.5
+		let config = new Jolt.TaperedCapsuleShapeSettings(l, r1, r2, null)
+        shape = config.Create().Get()
+        Jolt.destroy(config)
+        break
+    }
+    case 6: {                       // Static compound shape
         let config = new Jolt.StaticCompoundShapeSettings()
         let l = x[0], r2 = x[1] * 0.5, r1 = r2 * 0.5
         config.AddShape(
@@ -121,12 +116,10 @@ function jolt_update(jolt) {
     console.log(v)                          /// * debug trace
     
     const t = v[0], c = v[1]|0              ///> t, color
-    const x = v[2], s = v[3]                ///> geometry, shape
+    const x = v[2], ds= v[3]                ///> geometry, shape dynaset
     
     const msh   = t=='mesh'
-    const pos   = new Jolt.RVec3(s[1], s[2], s[3])
-    const rot   = get_q4(s[4], s[5], s[6], s[7])
-    const shape = get_shape(msh ? 0 : s[0]|0, x)
+    const shape = get_shape(msh ? 0 : ds[0]|0, x)
     
-    jolt.add(shape, pos, rot, c, msh)
+    jolt.add(shape, ds, c, msh)
 }
--- docs/ref/weforth_worker.js
+++ docs/ref/weforth_worker.js
@@ -2,83 +2,140 @@
 /// @file
 /// @brief weForth - worker proxy to weforth.js (called by weforth.html)
 ///
-Module = { print: e=>postMessage([ 'txt', e ]) }
+Module = { print: s=>postMessage(['txt', s]) } ///> WASM print interface => output queue
+
 var vm_dict_len = 0
-var vm_mem_addr = 0
+var vm_boot_idx = 0
 
-importScripts('weforth_helper.js')  /// * vocabulary handler
-importScripts('weforth.js')         /// * load js emscripten created
+importScripts('weforth_helper.js')             ///> vocabulary handler
+importScripts('weforth.js')                    ///> load js emscripten created
 
-function send_ss() {
+function get_ss() {
+    const FX = v=>Number.isInteger(v)          ///> precision control macro
+          ? v.toString(base)
+          : Math.round(v*100000)/100000        /// * better than toFixed()
     const wa  = wasmExports
     const base= wa.vm_base()
+    const toa = (p, n)=> wa.vm_dflt()
+        ? new Float32Array(wa.memory.buffer, p, n)
+        : (base==10
+           ? new Int32Array(wa.memory.buffer, p, n)
+           : new Uint32Array(wa.memory.buffer, p, n))
     const len = wa.vm_ss_idx()>0 ? wa.vm_ss_idx() : 0
-    const ss  = base==10
-          ? new Int32Array(wa.memory.buffer, wa.vm_ss(), len)
-          : new Uint32Array(wa.memory.buffer, wa.vm_ss(), len)
-    const top = base==10
-          ? new Int32Array(wa.memory.buffer, wa.top, 1)
-          : new Uint32Array(wa.memory.buffer, wa.top, 1)
-    let div = []
-    ss.forEach(v=>div.push(v.toString(base)))
-    div.push(top[0].toString(base))
-    postMessage([ 'ss', '[ ' + div.join(' ') + ' ]' ])
+    const ss  = toa(wa.vm_ss(), len)
+    const top = toa(wa.top, 1)
+    let   div = []
+    ss.forEach(v=>div.push(FX(v)))
+    div.push(FX(top[0]))
+    
+    return '[ '+div.join(' ')+' ]'
 }
-function send_dict() {
+function get_dict(usr=false) {
     const wa  = wasmExports
     const len = wa.vm_dict_idx()
-    if (vm_dict_len == len) return             /// * dict no change, skip
+    if (vm_dict_len == len) return ''     /// * dict no change, use cached
     
     vm_dict_len = len
     const dict = Module.cwrap('vm_dict', 'string', ['number'])
-    let nlst = []                              ///< built-in words list
-    let clst = null                            ///< colon word list
-    for (let i = 0; i < len; ++i) {
+    let lst = []                          ///< built-in words list
+    for (let i = usr ? vm_boot_idx : 0; i < len; ++i) {
         let nm = dict(i)
-        if (clst) clst.push(nm)
-        else {
-            if (nm[0]!='_') nlst.push(nm)
-            if (nm == 'boot') clst = []
-        }
+        if (nm=='boot') vm_boot_idx = i+1 ///< capture the start of colon words
+        if (nm[0] != '_') lst.push(nm)    ///< collect words
     }
-    postMessage([ 'dc', voc_tree(nlst) ])
-    postMessage([ 'us', colon_words(clst) ])
+    return usr ? colon_words(lst) : voc_tree(lst)
 }
-function send_mem(off, len) {
-    const wa = wasmExports
-    const hx = '0123456789ABCDEF'
-    const h2 = v=>hx[(v>>4)&0xf]+hx[v&0xf]
-    const h4 = v=>h2(v>>8)+h2(v)
+function get_mem(off, len) {
+    const wa  = wasmExports
     const adr = wa.vm_mem() + off
-    const mem = new Uint8Array(wa.memory.buffer, adr, len)
-    const n   = (off + len + 0x10) & ~0xf
-    let div = '', bt = '', tx = ''
-    for (let j = off&~0xf; j < n; j+=0x10) {
-        for (let i = 0; i < 0x10; i++) {
-            let ch = mem[i+j] || 0
-            bt += `${hx[ch>>4]}${hx[ch&0xf]}`
-            bt += ((i & 0x3)==3) ? '  ' : ' '
-            tx += (ch < 0x20) ? '_' : String.fromCharCode(ch)
-        }
-        div += h4(j) + ': ' + bt + tx + '\n'
-        bt = '', tx = ''
+    return new Uint8Array(wa.memory.buffer, adr, len)   /// CC: freed by caller?
+}
+
+let dump_mem0 = ''                                      /// memory cache
+function dump(mem, off) {
+    const hx  = '0123456789ABCDEF'
+    const h2  = v=>hx[(v>>4)&0xf]+hx[v&0xf]
+    const h4  = v=>h2(v>>8)+h2(v)
+    if (dump_mem0.length != mem.length) {               /// check buffer size
+        dump_mem0 = new Uint8Array(mem.length)          /// free and realloc
     }
-    postMessage([ 'mm', div ])
+    let div = ''
+    for (let j = 0; j < mem.length; j+=0x10) {
+        let bt = '', tx = '', en = 0
+        for (let i = 0; i < 0x10; i++) {
+            let c0 = dump_mem0[j + i] || 0
+            let c  = dump_mem0[j + i] = mem[j + i] || 0 ///> also cache the char
+            if (!en && c != c0) {
+                bt += '<i>'; tx += '<i>'; en = 1        /// * enter i element
+            }
+            else if (en && c == c0) {
+                bt += '</i>'; tx += '</i>'; en = 0      /// * exit <i> element
+            }
+            bt += `${hx[c>>4]}${hx[c&0xf]}`
+            bt += ((i & 0x3)==3) ? '  ' : ' '
+            tx += (c < 0x20) ? '_' : String.fromCharCode(c)
+        }
+        if (en) { bt += '</i>'; tx += '</i>' }
+        div += h4(off+j) + ': ' + bt + tx + '\n'
+    }
+    return div
+}
+function get_px(v) {
+    console.log(v)
+    const op = v[0], fg = v[1]|0  ///> opcode, foreground-color
+    const px = v[2]|0             ///> offsets to geometry buffer
+    const ps = v[3]|0             ///> offset to shape buffer
+    const wa = wasmExports
+    const mem= wa.vm_mem()
+    ///
+    /// references to WASM ArrayBuffer without copying (fast, < 1ms)
+    ///
+    const x0 = new Float32Array(wa.memory.buffer, mem+px, 3)  ///> x1, x2, x3
+    const s0 = new Float32Array(wa.memory.buffer, mem+ps, 8)  ///> id, pos, rot
+    ///
+    /// convert to transferable objects (F64 takes 2ms, F32 5ms)
+    ///
+    const x1 = new Float64Array(x0)
+    const s1 = new Float64Array(s0)
+    return [ op, fg, x1, s1, v[4], Date.now()-v[4] ]
 }
 ///
 /// worker message pipeline to main thread
 ///
-self.onmessage = function(e) {                    /// * link worker input port
+/// @note: serialization, i.e. structuredClone(), is slow (24ms)
+///        so prebuild a transferable object is much faster (~5ms)
+///
+self.onmessage = function(e) {                        ///> worker input message queue
     let k = e.data[0], v = e.data[1]
+    const post = (v)=>postMessage([k, v])             ///> macro to response to front-end
     switch (k) {
     case 'cmd':
         let forth =
             Module.cwrap('forth', null, ['number', 'string'])
-        forth(0, v);                  break
-    case 'ss' : send_ss();            break
-    case 'dc' : send_dict();          break
-    case 'mm' : send_mem(v[0], v[1]); break
-    case 'ui' : send_ui();            break
-    default: postMessage('unknown type');
+        forth(0, v)                                   /// * calls Module.print
+        break
+    case 'dc' : post(get_dict());            break
+    case 'usr': post(get_dict(true));        break
+    case 'ss' : post(get_ss());              break
+    case 'dm' :                                       /// * dump memory
+        const idx = v[0], n = v[1]
+        const here= wasmExports.vm_mem_idx()
+        const len = (n + 0x10) & ~0xf                 ///> 16-byte blocks
+        const off = idx < 0                           ///> idx < 0 => from 'HERE'
+            ? (here > len ? here - len : 0)
+            : idx
+        const ma  = get_mem(off & ~0xf, len)          ///> get memory ref
+        post(dump(ma, off));                 break
+    case 'mm' :
+        const mm = get_mem(v[0], v[1])                ///> fetch memory block
+        postMessage(                                  /// * to front-end, transfer
+            [ k, mm ],
+            [ mm.buffer ]);                  break
+    case 'px' :
+        const px = get_px(v)                          ///> fetch px values from Forth
+        postMessage(                                  ///> to front-end, transfer
+            [ k, px ],
+            [ px[2].buffer, px[3].buffer ]); break
+    default   : post('unknown type');
     }
 }
