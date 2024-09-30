38263be9c80b600be0f12f62a96aafb465a2b3de
gnii <chochain@yahoo.com>
2024-09-29 19:03:35 -0700

    ~/tests/forth/jolt.fs refactor 2PI,SMAX,fg,color => shapes.fs

--- tests/forth/jolt.fs
+++ tests/forth/jolt.fs
@@ -3,9 +3,6 @@
 : 3! >r r@ 8 + ! r@ 4 + ! r> ! ;       \ ( x y z a -- )
 : 4! >r r@ 12 + ! r> 3! ;              \ ( x y z w a -- )
 \ shape types and default colors
-6.2832 constant 2PI                    \ 2*PI (for degree => radian calc)
-6      constant SMAX                   \ shape 1:ball, 2:box, 3:cynlinder,
-                                       \       4:capsule, 5:tapered capsule, 6:dumbbell
 15 constant DSZ                        \ size of DYNASET (15 cells)
 : DYNASET create DSZ cells allot ;     \ ( "name" -- [ id, t, pos[3], rot[4], v[3], av[3] ] )
 : .T! 4 + ! ;                          \ set shape type
@@ -16,17 +13,13 @@
 \ variables
 V3       px                            \ [x0, x1, x2], parameters for shapes
 DYNASET  ds                            \ dynamic setting [id,pos[3],rot[4],v[3],av[3]]
-create fg                              \ default colors for mesh and shapes
-  $f0fff0 , $f04040 , $a0a0f0 , $f0ff40 ,
-  $80f080 , $f0d080 , $f0a0f0 ,
-: color ds 4 + @ cells fg + @ ;        \ fetch shape color (defined in ds[1])
-\ create mesh floor (id=0, shape=0)
+\ create sandbox (id=0, shape=0)
 : sandbox
   40 1 0.8 px 3!                       \ 40x40 mesh with cell size 1, max height=0.8
-  0        ds !                            \ id = 0
-  0        ds .T!                          \ shape = 0
-  0 -5 0   ds .P!                          \ pos xyz=[0,-5,0]
-  0 0 0 1  ds .R!                          \ rot xyzw=[0,0,0,1]
-  color 3 px DSZ ds                    \ get color[0], gemoetry, shape config
-  s" mesh %x %p %p" JS ;               \ foreward to front-end thread
+  0        ds !                          \ id = 0
+  0        ds .T!                        \ shape = 0
+  0 -5 0   ds .P!                        \ pos xyz=[0,-5,0]
+  0 0 0 1  ds .R!                        \ rot xyzw=[0,0,0,1]
+  $f0fff0 3 px DSZ ds                    \ color, gemoetry, shape config
+  s" sandbox %x %p %p" JS ;            \ foreward to front-end thread
 .( Jolt.fs loaded )
--- tests/forth/shapes.fs
+++ tests/forth/shapes.fs
@@ -2,6 +2,9 @@
 s" forth/jolt.fs" included             \ load jolt core
 sandbox                                \ display meshed floor
 \ randomized parameters
+6.2832 constant 2PI                    \ 2*PI (for degree => radian calc)
+6  constant SMAX                       \ shape 1:ball, 2:box, 3:cynlinder, 4:capsule
+                                       \       5:tapered capsule, 6:dumbbell
 : rx 2* rnd 0.5 - * ;                  \ ( n -- n' ) random with range [-n, n)
 : rnd_bdy ( id -- ) ds !               \ create a randam shape config with given id
   rnd SMAX * 1+ int ds .T!                 \ shape
@@ -13,6 +16,10 @@ sandbox                                \ display meshed floor
 : rnd_w 1 rx rnd           1 rx ds .W! ;   \ angular velocity[x, y, z]
 \ random shapes creation
 variable id 0 id !                     \ object id
+create fg                              \ default colors for shapes
+  $f0fff0 , $f04040 , $a0a0f0 , $f0ff40 ,
+  $80f080 , $f0d080 , $f0a0f0 ,
+: color ds 4 + @ cells fg + @ ;        \ fetch shape color (defined by ds[1])
 : one ( id -- )                        \ create one random body ( id should > 0 )
   1 id +! id @                         \ fetch id
   rnd_bdy rnd_geo rnd_v rnd_w          \ create random shape, geometry, velocities
