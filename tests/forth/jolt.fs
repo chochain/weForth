07d7a80fcd3a0352a111a022a713f7d7ea071c5f
gnii <chochain@yahoo.com>
2024-10-16 09:26:10 -0700

    ~/tests/forth/jolt.fs create struct/field

--- tests/forth/jolt.fs
+++ tests/forth/jolt.fs
@@ -1,27 +1,31 @@
 \ weForth demo - Jolt Core
-6.2832 constant 2PI                    \ for radian => degree
-: rad ( d -- r ) 2PI * 360 / ;         
-: V3 create 3 cells allot ;            \ geometry/vec3
-: 3! >r r@ 8 + ! r@ 4 + ! r> ! ;       \ ( x y z a -- )
-: 4! >r r@ 12 + ! r> 3! ;              \ ( x y z w a -- )
-\ shape types and default colors
-15 constant DSZ                        \ size of DYNASET (15 cells)
-: DYNASET create DSZ cells allot ;     \ ( "name" -- [ id, t, pos[3], rot[4], v[3], av[3] ] )
-: .T! 4 + ! ;                          \ set shape type
-: .P! 8 + 3! ;                         \ position ( x y z a -- )
-: .R! 20 + 4! ;                        \ rotation ( x y z w a -- )
-: .V! 36 + 3! ;                        \ linear velocity  ( x y z a -- )
-: .W! 48 + 3! ;                        \ angular velocity ( x y z a -- )
+6.2832 constant 2PI
+: rad ( d -- r ) 2PI * 360 / ;          \ radian => degree
+: V3 create 3 cells allot ;             \ geometry/vec3
+: 3! >r r@ 2 th ! r@ 1 th ! r> ! ;      \ ( x y z a -- )
+: 4! >r r@ 3 th ! r> 3! ;               \ ( x y z w a -- )
+\ struct/field constructor
+0 constant struct
+: field ( struct-sz fsz -- )
+  create over , + does> @ + ;
 \ variables
-V3       px                            \ [x0, x1, x2], parameters for shapes
-DYNASET  ds                            \ dynamic setting [id,pos[3],rot[4],v[3],av[3]]
+V3 px                                   \ [x0,x1,x2], params for shapes
+struct                                  \ anonymous struct
+  1 cells field .id                       \ object id
+  1 cells field .type                     \ type or color
+  3 cells field .pos                      \ position [x, y, z]
+  4 cells field .rot                      \ rotation [x, y, z, w]
+  3 cells field .lv                       \ linear velocity [x, y, z]
+  3 cells field .av                       \ angular velociy [x, y, z]
+create ds allot                         \ create DYNASET
+here ds - 1 cells / constant DSZ        \ size of DYNASET (15 cells)
 \ create sandbox (id=0, shape=0)
 : sandbox
-  40 1 0.8 px 3!                       \ 40x40 mesh with cell size 1, max height=0.8
-  0        ds !                          \ id = 0
-  0        ds .T!                        \ shape = 0
-  0 -5 0   ds .P!                        \ pos xyz=[0,-5,0]
-  0 0 0 1  ds .R!                        \ rot xyzw=[0,0,0,1]
-  $f0fff0 3 px DSZ ds                    \ color, gemoetry, shape config
+  40 1 0.8 px 3!                        \ 40x40 mesh with cell size 1, max height=0.8
+  0        ds .id   !                     \ id = 0
+  0        ds .type !                     \ shape = 0
+  0 -5 0   ds .pos  3!                    \ pos xyz=[0,-5,0]
+  0 0 0 1  ds .rot  4!                    \ rot xyzw=[0,0,0,1]
+  $f0fff0 3 px DSZ ds                     \ color, gemoetry, shape config
   s" sandbox %x %p %p" JS ;            \ foreward to front-end thread
 .( Jolt.fs loaded )
