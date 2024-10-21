6ee9926c2408b2b2bf439dd62a911b1fdafa20d7
gnii <chochain@yahoo.com>
2024-10-21 16:22:49 -0700

    ~/tests/sha256.cpp add test cases

--- tests/sha256.cpp
+++ tests/sha256.cpp
@@ -13,7 +13,7 @@ class SHA256 {
     
     U32 _tot;
     U32 _len;
-    U8  _block[2 * BLOCK_SZ];
+    U8  _data[2 * BLOCK_SZ];
     U32 _h[8];
     
     void _transform(const U8 *msg, U32 nblock);
@@ -28,10 +28,10 @@ public:
 #define ROL(x, n)    ((x << n) | (x >> ((sizeof(x) << 3) - n)))
 #define CH(x, y, z)  ((x & y) ^ (~x & z))
 #define MAJ(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
-#define E0(x)        (ROR(x,  2) ^ ROR(x, 13) ^ ROR(x, 22))
-#define E1(x)        (ROR(x,  6) ^ ROR(x, 11) ^ ROR(x, 25))
 #define S0(x)        (ROR(x,  7) ^ ROR(x, 18) ^ (x >> 3))
 #define S1(x)        (ROR(x, 17) ^ ROR(x, 19) ^ (x >> 10))
+#define E0(x)        (ROR(x,  2) ^ ROR(x, 13) ^ ROR(x, 22))
+#define E4(x)        (ROR(x,  6) ^ ROR(x, 11) ^ ROR(x, 25))
 #define UNPACK32(x, str)                  \
 {                                         \
     *((str) + 3) = (U8)((x)      );       \
@@ -68,23 +68,21 @@ const U32 SHA256::K[64] = { //UL = uint32
 
 void SHA256::_transform(const U8 *msg, U32 nblock)
 {
-    U32 m[64], h[8], t1, t2;
-    const U8 *sub_block;
+    U32 w[64], h[8];
     for (int i = 0; i < (int)nblock; i++) {
-        sub_block = msg + (i << 6);
+        const U8 *p = msg + (i << 6);
         for (int j = 0; j < 16; j++) {
-            PACK32(&sub_block[j << 2], &m[j]);
+            PACK32(&p[j << 2], &w[j]);
         }
         for (int j = 16; j < 64; j++) {
-            m[j] =  S1(m[j -  2]) + m[j -  7] + S0(m[j - 15]) + m[j - 16];
+            w[j] =  S1(w[j -  2]) + w[j -  7] + S0(w[j - 15]) + w[j - 16];
         }
         for (int j = 0; j < 8; j++) {
-            h[j] = _h[j];
+            h[j] = _h[j];   // a,b,..,h
         }
         for (int j = 0; j < 64; j++) {
-            t1 = h[7] + E1(h[4]) + CH(h[4], h[5], h[6])
-                + K[j] + m[j];
-            t2 = E0(h[0]) + MAJ(h[0], h[1], h[2]);
+            U32 t1 = h[7] + E4(h[4]) + CH(h[4], h[5], h[6]) + K[j] + w[j];
+            U32 t2 = E0(h[0]) + MAJ(h[0], h[1], h[2]);
 
             h[7] = h[6];
             h[6] = h[5];
@@ -117,22 +115,24 @@ void SHA256::init()
  
 void SHA256::update(const U8 *msg0, U32 len)
 {
-    U32 tmp_len = BLOCK_SZ - _len;
-    U32 rem_len = len < tmp_len ? len : tmp_len;
-    memcpy(&_block[_len], msg0, rem_len);
+    U32 delta   = BLOCK_SZ - _len;
+    U32 rem_len = len < delta ? len : delta;
+    memcpy(&_data[_len], msg0, rem_len);
     
-    if (_len + len < BLOCK_SZ) {
+    if (len < delta) {
         _len += len;
         return;
     }
+    _transform(_data, 1);
+    
+    const U8 *msg1 = msg0 + rem_len;
     U32 new_len    = len - rem_len;
     U32 nblock     = new_len / BLOCK_SZ;
-    const U8 *msg1 = msg0 + rem_len;
-    _transform(_block, 1);
+    printf("nblock=%d\n", nblock);
     _transform(msg1, nblock);
     
     rem_len = new_len % BLOCK_SZ;
-    memcpy(_block, &msg1[nblock << 6], rem_len);
+    memcpy(_data, &msg1[nblock << 6], rem_len);
     
     _len = rem_len;
     _tot += (nblock + 1) << 6;
@@ -143,11 +143,11 @@ void SHA256::final(U8 *digest)
     U32 nblock = (1 + ((BLOCK_SZ - 9) < (_len % BLOCK_SZ)));
     U32 len_b  = (_tot + _len) << 3;
     U32 pm_len = nblock << 6;
-    memset(_block + _len, 0, pm_len - _len);
+    memset(_data + _len, 0, pm_len - _len);
     
-    _block[_len] = 0x80;
-    UNPACK32(len_b, _block + pm_len - 4);
-    _transform(_block, nblock);
+    _data[_len] = 0x80;
+    UNPACK32(len_b, _data + pm_len - 4);
+    _transform(_data, nblock);
     
     for (int i = 0 ; i < 8; i++) {
         UNPACK32(_h[i], &digest[i << 2]);
@@ -174,14 +174,28 @@ string sha256(SHA256 &ctx, string input)
 int main(int argc, char *argv[])
 {
     const string gold[] = {
-        "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
-        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-        "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"
+      "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+      "cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1",
+      "ab64eff7e88e2e46165e29f2bce41826bd4c7b3552f6b382a9e7d3af47c245f8",
+      "f08a78cbbaee082b052ae0708f32fa1e50c5c421aa772ba5dbb406a2ea6be342",
+      "0ab803344830f92089494fb635ad00d76164ad6e57012b237722df0d7ad26896",
+      "e4326d0459653d7d3514674d713e74dc3df11ed4d30b4013fd327fdb9e394c26",
+      "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592",
+      "2ce675bd3b70e104d696d1b25bf3d42b2b45cd776d4f590f210f12c44bf473d5"
     };
     const string input[] = {
         "abc",
         "",
-        "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
+        "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+        "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+        "This is exactly 64 bytes long, not counting the terminating byte",
+        "For this sample, this 63-byte string will be used as input data",
+        "And this textual data, astonishing as it may appear, is exactly 128 bytes in length, as are both SHA-384 and SHA-512 block sizes",
+        "By hashing data that is one byte less than a multiple of a hash block length (like this 127-byte string), bugs may be revealed.",
+        "The quick brown fox jumps over the lazy dog",
+        "This test tries to use the n-block utility from the hash library and as a matter of fact we're trying to get only 128 characters"
     };
     SHA256 ctx = SHA256();
     for (int i=0; i< sizeof(input)/sizeof(string); i++) {
